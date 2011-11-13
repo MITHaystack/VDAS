8e363ba42eb04e36261a3af841b3cd9bdd534c1b
David Lapsley <del@haystack.mit.edu>
2011-11-13 03:47:21 -0500

    Refactored build system.

---
+++ src/drs/src/TestClient.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+## Copyright 2011 MIT Haystack Observatory
+##
+## This file is part of Mark6 VDAS.
+##
+## Mark6 VDAS is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2 of the License.
+##
+## Mark6 VDAS is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Mark6 VDAS.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+Author:   del@haystack.mit.edu
+Date:     11/12/2011
+Description:
+'''
+
+
+import unittest
+
+class TestClient(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def test_construct(self):
+    pass
+
+if __name__ == '__main__':
+  suite = unittest.TestLoader().loadTestsFromTestCase(TestClient)
+  unittest.TextTestRunner(verbosity=2).run(suite)
+
--- src/drs/src/drs-client.py
+++ src/drs/src/drs-client.py
@@ -2,20 +2,19 @@
 
 ## Copyright 2011 MIT Haystack Observatory
 ##
-## This file is part of dimino6.
+## This file is part of Mark6 VDAS.
 ##
-## dimino6 is free software: you can redistribute it and/or modify
+## Mark6 VDAS is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
+## the Free Software Foundation, version 2 of the License.
 ##
-## dimino6 is distributed in the hope that it will be useful,
+## Mark6 VDAS is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with dimino6.  If not, see <http://www.gnu.org/licenses/>.
+## along with Mark6 VDAS.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
 Author:   del@haystack.mit.edu
